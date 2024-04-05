dfa8d53f3467a78d597979149539e1cbc7321865
Alexander Nicke <alexander.nicke@sap.com>
2024-04-05 17:27:22 +0200

    v1.0

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-function toggleRecord() {
-    // record user audio and save it to the #audio element
+var chunks = [];
+var mediaRecorder;
+var startTime;
+var angle;
+var startButton;
+var interval;
+
+//onload
+document.addEventListener('DOMContentLoaded', function () {
+    startButton = document.getElementById('start');
+    setupMediaRecorder();
+});
+
+function startRecording() {
+    chunks = [];
+    mediaRecorder.start();
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
+}
+
+function setupMediaRecorder() {
     navigator.mediaDevices.getUserMedia({
         audio: true
     }).then(function (stream) {
-        const mediaRecorder = new MediaRecorder(stream);
-        var audio = document.createElement('audio');
-
-        var chunks = [];
-
-        mediaRecorder.start();
+        mediaRecorder = new MediaRecorder(stream);
 
         mediaRecorder.addEventListener('dataavailable', function (event) {
             chunks.push(event.data);
         });
 
         mediaRecorder.addEventListener('stop', function () {
-            // reverse audio playback in chunks before playing
-            glboalChunks = chunks;
             var blob = new Blob(chunks, {
                 type: 'audio/wav'
             });
 
-            var buffer = blob.arrayBuffer().then(function (buffer) {
+            blob.arrayBuffer().then(function (buffer) {
                 var context = new AudioContext();
                 context.decodeAudioData(buffer, function (buffer) {
                     var source = context.createBufferSource();
@@ -29,25 +43,39 @@ function toggleRecord() {
                     source.buffer = buffer;
                     source.connect(context.destination);
                     source.start();
+
                 });
             });
-
-
-            // var audioURL = URL.createObjectURL(blob);
-            // audio.src = audioURL;
-            // audio.play();
-
-
         });
+    });
+}
 
-        document.getElementById('start').addEventListener('click', function () {
-            if (mediaRecorder.state === 'recording') {
-                mediaRecorder.stop();
-                this.textContent = '||2';
-            } else {
-                mediaRecorder.start();
-                this.textContent = 'Stop';
-            }
-        });
-    })
+function toggleRecord() {
+    if (mediaRecorder.state === 'recording') {
+        stopRecording();
+        rotateBack();
+    } else {
+        startRecording();
+        startTurning();
+    }
+}
+
+
+function startTurning() {
+    angle = 0;
+    startTime = new Date().getTime();
+    interval = setInterval(function () {
+        angle = (angle + 10) % 360;
+        startButton.style.transform = 'rotate(' + angle + 'deg)';
+    }, 1000);
+}
+
+function rotateBack() {
+    clearInterval(interval);
+    var duration = new Date().getTime() - startTime;
+
+    // rotate back to 0 deg in duration ms. use global angle variable
+    startButton.style.transition = 'transform ' + duration + 'ms linear';
+    startButton.style.transform = 'rotate(0deg)';
+
 }
